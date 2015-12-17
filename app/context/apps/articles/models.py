1877a972930155e4f4783025f03423dafe0af8ea
Abhi Agarwal <me@abhiagarwal.com>
2015-12-16 23:26:30 -0500

    Basic URL validation

--- app/context/apps/articles/models.py
+++ app/context/apps/articles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Article(models.Model):
     name = models.TextField(blank=False, max_length=100)
-    url = models.URLField(blank=False)
+    url = models.URLField(blank=False, unique=True, max_length=500)
 
     def __unicode__(self):
         return self.name
--- app/context/apps/articles/serializers.py
+++ app/context/apps/articles/serializers.py
@@ -1,3 +1,5 @@
+from urlparse import urlparse
+
 from .models import Article
 from rest_framework import serializers
 
@@ -8,6 +10,9 @@ class ArticlerSerializer(serializers.HyperlinkedModelSerializer):
     def create(self, data):
         # Can do the Machine Learning/NLP tasks for an Article here
         # before it is added as a new row.
+        if 'url' in data:
+            url = urlparse(data['url'])
+            data['url'] = url.scheme + "://" + url.netloc + url.path
         return Article.objects.create(**data)
 
     class Meta:
