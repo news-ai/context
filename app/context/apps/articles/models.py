cda66826faaf2b2765b56dbc108814ef6b81eaa1
Abhi Agarwal <me@abhiagarwal.com>
2015-12-16 22:51:42 -0500

    Adding URL and create method to serializer

--- app/context/apps/articles/models.py
+++ app/context/apps/articles/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Article(models.Model):
     name = models.TextField(blank=False, max_length=100)
+    url = models.URLField(blank=False)
 
     def __unicode__(self):
         return self.name
--- app/context/apps/articles/serializers.py
+++ app/context/apps/articles/serializers.py
@@ -1,7 +1,15 @@
 from .models import Article
 from rest_framework import serializers
 
+
 class ArticlerSerializer(serializers.HyperlinkedModelSerializer):
+
+    # Defining behavior of when a new Article is added
+    def create(self, data):
+        # Can do the Machine Learning/NLP tasks for an Article here
+        # before it is added as a new row.
+        return Article.objects.create(**data)
+
     class Meta:
         model = Article
-        fields = ('name',)
+        fields = ('name', 'url',)
