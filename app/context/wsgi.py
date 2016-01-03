c75f47985de4abf6cd6d1a8a74b16fd9b0d58038
Abhi Agarwal <me@abhiagarwal.com>
2016-01-04 00:20:57 +0700

    Updating wsgi

--- app/context/wsgi.py
+++ app/context/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "context.settings")
-
+env = os.getenv('CONTEXT_ENVIRONMENT') or 'dev'
+if env not in ('dev', 'stage', 'prod'):
+    env = 'dev'
+os.environ.setdefault("CONXTEXT_ENVIRONMENT", env)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "context.settings.%s" % env)
 application = get_wsgi_application()
