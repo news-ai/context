98f5675dbd09fd6fc1796228ab50d9e19e578c7c
Abhi Agarwal <me@abhiagarwal.com>
2016-01-04 15:25:15 +0700

    Changing order

--- app/context/settings/common.py
+++ app/context/settings/common.py
@@ -96,9 +96,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAdminUser'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
