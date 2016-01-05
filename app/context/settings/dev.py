d04da45dba783fb0c64c2cb182905466211291bf
Abhi Agarwal <me@abhiagarwal.com>
2016-01-05 23:18:27 +0700

    Adding django debug

--- app/context/settings/dev.py
+++ app/context/settings/dev.py
@@ -26,3 +26,5 @@ REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
     'rest_framework.renderers.JSONRenderer',
     'rest_framework.renderers.BrowsableAPIRenderer',
 )
+
+INSTALLED_APPS += ('debug_toolbar',)
