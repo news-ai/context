5123e199793aa1a894872173854a4dd8ee4dc9c0
Abhi Agarwal <me@abhiagarwal.com>
2016-02-05 11:58:01 -0500

    Changes to models

--- app/context/apps/articles/models.py
+++ app/context/apps/articles/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Publisher(models.Model):
     name = models.TextField(blank=False, max_length=100)
     short_name = models.TextField(blank=False, max_length=5)
-    url = models.URLField(blank=False, unique=True, max_length=500)
+    url = models.URLField(blank=False, unique=True, max_length=255)
     has_paywall = models.BooleanField(blank=False, default=False)
 
     def __unicode__(self):
