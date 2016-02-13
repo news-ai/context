8fd2ec351faae60f0d51b0dbb2dfbfc6f3bcedbc
Abhi Agarwal <me@abhiagarwal.com>
2016-02-13 14:46:28 -0800

    Adding microservice model

--- app/context/apps/users/models.py
+++ app/context/apps/users/models.py
@@ -7,6 +7,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User)
     website = models.URLField(blank=True)
     picture = models.ImageField(upload_to='profile_images', blank=True)
+    is_microservice_user = models.BooleanField(blank=False, default=False)
 
     def __unicode__(self):
         return self.user.username
