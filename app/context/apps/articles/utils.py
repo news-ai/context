59b75928070aad22fa70931c70265e49b157ae48
Abhi Agarwal <me@abhiagarwal.com>
2016-02-19 17:14:20 -0500

    Formatting and removing unused dependencies

--- app/context/apps/articles/utils.py
+++ app/context/apps/articles/utils.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from urlparse import urlparse
-from newspaper import Article
 
-from bs4 import BeautifulSoup
-import requests
+from newspaper import Article
 
 
 def url_validate(url):
     url = urlparse(url)
-    return (url.scheme + "://" + url.netloc + url.path, url.scheme + "://" + url.netloc)
+    return (
+        url.scheme + "://" + url.netloc +
+        url.path, url.scheme + "://" + url.netloc
+    )
 
 
 def get_article(url):
