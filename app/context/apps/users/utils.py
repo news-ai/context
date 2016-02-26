a09a8b25cb57710c6d903075d2d13811e0438d3c
Abhi Agarwal <me@abhiagarwal.com>
2016-02-25 21:31:17 -0500

    Switching to ForeignKey

--- app/context/apps/users/utils.py
+++ app/context/apps/users/utils.py
@@ -8,15 +8,15 @@ def check_company_auth(strategy, details, user=None, *args, **kwargs):
     except UserProfile.DoesNotExist:
         user_profile = None
 
-    if not user_profile and details['email']:
-        # Find or create company domain
-        split_email = details['email'].split('@')
-        company_name = split_email[1].split('.')[0]
-        company_profile, created = Company.objects.get_or_create(
-            name=company_name,
-            email_extension=split_email[1],
-        )
+    # Find or create company domain
+    split_email = details['email'].split('@')
+    company_name = split_email[1].split('.')[0]
+    company_profile, created = Company.objects.get_or_create(
+        name=company_name,
+        email_extension=split_email[1],
+    )
 
+    if not user_profile and details['email']:
         user_profile, created = UserProfile.objects.get_or_create(
             user=user,
             company=company_profile,
