6776893aa7a9e7348e1e6c443c2576a409f428a0
Abhi Agarwal <me@abhiagarwal.com>
2016-03-07 13:41:47 -0500

    Adding to admin site

--- app/context/apps/users/admin.py
+++ app/context/apps/users/admin.py
@@ -3,7 +3,9 @@
 from django.contrib import admin
 
 # Imports from app
-from .models import Company, UserProfile
+from .models import Company, UserProfile, Feature, Subscription
 
 admin.site.register(Company)
 admin.site.register(UserProfile)
+admin.site.register(Feature)
+admin.site.register(Subscription)
