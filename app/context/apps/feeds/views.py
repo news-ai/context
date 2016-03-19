268d095daa6929dc538b3e92731f8bb10b42f222
Abhi Agarwal <me@abhiagarwal.com>
2016-03-19 13:27:59 -0400

    Attempting new pagination

--- app/context/apps/feeds/views.py
+++ app/context/apps/feeds/views.py
@@ -2,6 +2,7 @@
 # Third-party app imports
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.pagination import LimitOffsetPagination
 
 # Imports from app
 from .permissions import FeedPermission
@@ -9,9 +10,16 @@ from context.apps.articles.models import Article
 from context.apps.articles.serializers import ArticlerSerializer
 
 
+# Custom pagination specifically for the feed
+class StandardResultsSetPagination(LimitOffsetPagination):
+    page_size = 20
+    max_page_size = 1000
+
+
 class FeedViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ArticlerSerializer
     permission_classes = (FeedPermission,)
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         path = self.request.path.split('/')
