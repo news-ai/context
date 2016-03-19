b88fbd848aabb7586230cb0a84f22bb824af7262
Abhi Agarwal <me@abhiagarwal.com>
2016-03-19 13:34:50 -0400

    Changing pagination to 20

--- app/context/settings/common.py
+++ app/context/settings/common.py
@@ -116,7 +116,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    'PAGE_SIZE': 20,
 }
 
 JWT_AUTH = {
