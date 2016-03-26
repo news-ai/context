32665ade5375540914df2b3edf44614d8d9b0c5a
Abhi Agarwal <me@abhiagarwal.com>
2016-03-26 14:52:02 -0400

    Removing Article from admin

--- app/context/apps/articles/admin.py
+++ app/context/apps/articles/admin.py
@@ -5,7 +5,6 @@ from django.contrib import admin
 # Imports from app
 from .models import Article, Author, Publisher, PublisherFeed, Topic
 
-admin.site.register(Article)
 admin.site.register(Author)
 admin.site.register(Publisher)
 admin.site.register(PublisherFeed)
