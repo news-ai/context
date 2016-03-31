5db19ce25d5183e65bd03ecbd46862cc84cc334e
Abhi Agarwal <me@abhiagarwal.com>
2016-03-31 13:53:19 -0400

    Removing datetime

--- app/context/apps/articles/managers.py
+++ app/context/apps/articles/managers.py
@@ -9,9 +9,4 @@ from django.db import models
 class ArticleManager(models.Manager):
 
     def articles_today_and_approved(self):
-        now = datetime.datetime.now()
-        twelve_hours = now - datetime.timedelta(hours=12)
-        return self.filter(added_at__range=(twelve_hours, now),
-                           is_approved=True,
-                           entities_processed=True
-                           )
+        return self.filter(is_approved=True, entities_processed=True).order_by('-added_at')
