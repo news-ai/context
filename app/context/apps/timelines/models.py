6e230b522f94c3ac589a074f1fcad2d3b1e78557
Abhi Agarwal <me@abhiagarwal.com>
2016-04-02 14:17:55 -0400

    Adding more fields to event information

--- app/context/apps/timelines/models.py
+++ app/context/apps/timelines/models.py
@@ -2,13 +2,20 @@
 # Core Django imports
 from django.db import models
 
+# Third-party app imports
+from django_countries.fields import CountryField
+
 # Imports from app
 from context.apps.articles.models import Article
+from context.apps.entities.models import Entity
 
 
 class Event(models.Model):
     name = models.TextField(blank=False, max_length=100)
-    articles = models.ManyToManyField(Article)
+    articles = models.ManyToManyField(Article, blank=True)
+    description = models.TextField(blank=True)
+    entities = models.ManyToManyField(Entity, blank=True)
+    for_country = CountryField(blank_label='(select country)', blank=True)
 
     def __unicode__(self):
         return self.name
