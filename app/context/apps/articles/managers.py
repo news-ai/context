3101917bf4aee2d8cc122c8f0a41e4246139e55b
Abhi Agarwal <me@abhiagarwal.com>
2016-04-17 18:59:10 -0400

    Adding exclusion of non entity scores

--- app/context/apps/articles/managers.py
+++ app/context/apps/articles/managers.py
@@ -9,4 +9,4 @@ from django.db import models
 class ArticleManager(models.Manager):
 
     def articles_today_and_approved(self):
-        return self.filter(is_approved=True, entities_processed=True).order_by('-added_at')
+        return self.filter(is_approved=True, entities_processed=True).exclude(entity_scores=None).order_by('-added_at')
