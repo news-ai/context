08eb7105933494cb37a6c247db4779e4b08e0b2a
Abhi Agarwal <me@abhiagarwal.com>
2016-04-24 18:44:53 -0400

    Adding token authentication

--- app/context/settings/common.py
+++ app/context/settings/common.py
@@ -113,8 +113,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAdminUser'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
