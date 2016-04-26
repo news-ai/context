da0fce21fbc0eb483781400ffad912916d6f7dd6
Abhi Agarwal <me@abhiagarwal.com>
2016-04-26 15:27:47 -0400

    Fixing return query

--- app/context/apps/general/views.py
+++ app/context/apps/general/views.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 # Third-party app imports
 from rest_framework import status
 from rest_framework.exceptions import PermissionDenied, NotFound, NotAuthenticated
@@ -7,11 +6,11 @@ from rest_framework.exceptions import PermissionDenied, NotFound, NotAuthenticat
 
 def general_response(request, base_model, uid):
     queryset = base_model.objects.all()
-    if len(queryset) is 0:
-        return queryset
     # Makes sure that the id that the user has entered is of an integer
     # value.
     if request.user and request.user.is_authenticated():
+        if len(queryset) is 0:
+            return queryset
         if uid and (isinstance(uid, unicode) or isinstance(uid, int)):
             resource = base_model.objects.filter(pk=uid)
             if resource:
