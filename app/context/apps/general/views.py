1afffad3f1a6e6de1a1c7298d377a89ecb1af11a
Abhi Agarwal <me@abhiagarwal.com>
2016-04-26 10:24:16 -0400

    Fixing permissions issue

--- app/context/apps/general/views.py
+++ app/context/apps/general/views.py
@@ -7,14 +7,14 @@ def general_response(request, base_model, uid):
         return queryset
     # Makes sure that the id that the user has entered is of an integer
     # value.
-    elif uid and (isinstance(uid, unicode) or isinstance(uid, int)):
-        resource = base_model.objects.filter(pk=uid)
-        if resource:
-            return resource
+    if request.user and request.user.is_authenticated():
+        if uid and (isinstance(uid, unicode) or isinstance(uid, int)):
+            resource = base_model.objects.filter(pk=uid)
+            if resource:
+                return resource
+            else:
+                raise NotFound("No matching resource found.")
         else:
-            raise NotFound("No matching resource found.")
-    else:
-        if request.user and request.user.is_authenticated():
             if request.user.is_staff:
                 return queryset
             else:
