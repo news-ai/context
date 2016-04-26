b32406bf9b337304cbba5f8e791c4c8464036ba0
Abhi Agarwal <me@abhiagarwal.com>
2016-04-26 10:19:16 -0400

    Fixing queryset filter

--- app/context/apps/general/views.py
+++ app/context/apps/general/views.py
@@ -1,4 +1,4 @@
-from rest_framework.exceptions import PermissionDenied, NotAuthenticated
+from rest_framework.exceptions import PermissionDenied, NotAuthenticated, NotFound
 
 
 def general_response(request, base_model, uid):
@@ -8,9 +8,11 @@ def general_response(request, base_model, uid):
     # Makes sure that the id that the user has entered is of an integer
     # value.
     elif uid and isinstance(uid, int):
-        article = queryset.filter(pk=uid)
-        if article:
-            return article
+        resource = base_model.objects.filter(pk=uid)
+        if resource:
+            return resource
+        else:
+            raise NotFound("No matching resource found.")
     else:
         if request.user and request.user.is_authenticated():
             if request.user.is_staff:
