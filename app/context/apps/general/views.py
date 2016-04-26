c0679dc2ee08b8c9b0e575935e06801d545470d3
Abhi Agarwal <me@abhiagarwal.com>
2016-04-26 09:57:00 -0400

    Raising exceptions :fire:

--- app/context/apps/general/views.py
+++ app/context/apps/general/views.py
@@ -1,3 +1,6 @@
+from rest_framework.exceptions import PermissionDenied, NotAuthenticated
+
+
 def general_response(request, base_model, uid):
     queryset = base_model.objects.all()
     if len(queryset) is 0:
@@ -9,6 +12,9 @@ def general_response(request, base_model, uid):
         if article:
             return article
     else:
-        if request.user and request.user.is_staff:
-            return queryset
-    return base_model.objects.none()
+        if request.user and request.user.is_authenticated():
+            if request.user.is_staff:
+                return queryset
+            else:
+                raise PermissionDenied("Forbidden.")
+    raise NotAuthenticated("Authentication Required.")
