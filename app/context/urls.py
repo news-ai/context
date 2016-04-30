99d9f0ef2e3194d74ebe16acfcb41d0fb791a337
Abhi Agarwal <me@abhiagarwal.com>
2016-04-30 13:16:13 -0400

    Moving to v1 versioning

--- app/context/urls.py
+++ app/context/urls.py
@@ -1,4 +1,5 @@
 # Core Django imports
+from django.views.generic.base import RedirectView
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -13,7 +14,8 @@ urlpatterns = [
     url(r'^docs/', include('rest_framework_swagger.urls')),
 
     # API & JSON Web Tokens
-    url(r'^api/', include(router.urls)),
+    url(r'^v1/api/', include(router.urls), name='v1'),
+    url(r'^api/', RedirectView.as_view(url='/v1/api/', permanent=True), name='api'),
     url(r'^api/jwt-token/', 'rest_framework_jwt.views.obtain_jwt_token'),
     url(r'^api/jwt-token-refresh/', 'rest_framework_jwt.views.refresh_jwt_token'),
     url(r'^api/jwt-token-verify/', 'rest_framework_jwt.views.verify_jwt_token'),
