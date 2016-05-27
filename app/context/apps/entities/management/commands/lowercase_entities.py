7f656296eb5f3e91f52c21024c2f310630821fe5
Abhi Agarwal <me@abhiagarwal.com>
2016-05-27 19:10:22 -0400

    Adding command to put a lowercase entity

---
+++ app/context/apps/entities/management/commands/lowercase_entities.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Core Django imports
+from django.db.models import Count
+from django.core.management.base import NoArgsCommand
+
+# Imports from app
+from context.apps.entities.models import Entity, EntityScore
+
+
+class Command(NoArgsCommand):
+
+    def handle_noargs(self, **options):
+        entities = Entity.objects.filter(name__iregex=r'^[a-z]+$')
+        for entity in entities:
+            if entity.name.islower():
+                entity.name = entity.name.title()
+                entity.save()
