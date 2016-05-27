25075e0c64677f36dd7d59b6ff98483c99261f17
Abhi Agarwal <me@abhiagarwal.com>
2016-05-27 17:11:32 -0400

    Attempting merge logic

--- app/context/apps/entities/management/commands/merge_entities.py
+++ app/context/apps/entities/management/commands/merge_entities.py
@@ -10,12 +10,26 @@ from context.apps.entities.models import Entity, EntityScore
 class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
+        # Find the most popular duplicate entity
         duplicate_entities = Entity.objects.values('name')
         duplicate_entities_count = duplicate_entities.annotate(
             Count('id')).order_by().filter(id__count__gt=1)
 
+        # What entity are we talking about
         single_entity_name = duplicate_entities_count[0]['name']
+        print 'Entity to merge:', single_entity_name
+
+        # Get the entire collection
         entity_collection = Entity.objects.filter(name=single_entity_name)
-        for entity in entity_collection:
+
+        # We want to merge the entities into the first one
+        first_entity = entity_collection[0]
+
+        # Loop through the extra entities
+        for entity in entity_collection[1:]:
             entity_scores = EntityScore.objects.filter(pk=entity.pk)
-            print len(entity_scores)
+            for entity_score in entity_scores:
+                # Set the entity_scores for that entity to the first entity
+                entity_score.entity = first_entity
+                entity_score.save()
+            entity.delete()
