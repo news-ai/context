d7ea79d49e113dd39b69352c7c2d939a13fbdfea
Abhi Agarwal <me@abhiagarwal.com>
2016-05-29 17:31:07 -0400

    Fixing merge entities count

--- app/context/apps/entities/management/commands/merge_entities.py
+++ app/context/apps/entities/management/commands/merge_entities.py
@@ -16,23 +16,22 @@ class Command(NoArgsCommand):
             Count('id')).order_by().filter(id__count__gt=1)
 
         while len(duplicate_entities_count) > 0:
+            # What entity are we talking about
+            print 'Duplicate left', len(duplicate_entities_count)
+            single_entity_name = duplicate_entities_count[0]['name']
+            print 'Entity to merge:', single_entity_name
 
-        # What entity are we talking about
-        print 'Duplicate left', len(duplicate_entities_count)
-        single_entity_name = duplicate_entities_count[0]['name']
-        print 'Entity to merge:', single_entity_name
+            # Get the entire collection
+            entity_collection = Entity.objects.filter(name=single_entity_name)
 
-        # Get the entire collection
-        entity_collection = Entity.objects.filter(name=single_entity_name)
+            # We want to merge the entities into the first one
+            first_entity = entity_collection[0]
 
-        # We want to merge the entities into the first one
-        first_entity = entity_collection[0]
-
-        # Loop through the extra entities
-        for entity in entity_collection[1:]:
-            entity_scores = EntityScore.objects.filter(pk=entity.pk)
-            for entity_score in entity_scores:
-                # Set the entity_scores for that entity to the first entity
-                entity_score.entity = first_entity
-                entity_score.save()
-            entity.delete()
+            # Loop through the extra entities
+            for entity in entity_collection[1:]:
+                entity_scores = EntityScore.objects.filter(pk=entity.pk)
+                for entity_score in entity_scores:
+                    # Set the entity_scores for that entity to the first entity
+                    entity_score.entity = first_entity
+                    entity_score.save()
+                entity.delete()
